181d2071f1ffe8b73cc409b170a0d14e6de30776
Adam Jönsson <adam.joensson90@gmail.com>
2022-04-01 16:05:13 +0200

    Added @BeforeAll and @BeforeEach for the test. Still bug present in the code

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,3 +1,5 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -5,9 +7,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
+    @BeforeAll
+    public static void init(){
+        System.out.println("BeforeAll init() method called");
+    }
+
+    @BeforeEach
+    public void initEach(){
+        System.out.println("BeforeEach initEach() method called");
+    }
+
     @Test
     @DisplayName("Add two numbers")
      void add() {
+        // To fix this bug, change .add to 2,2 and it should work.
         assertEquals(4, Calculator.add(1, 2));
     }
 
